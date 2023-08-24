d7b4a00d24e24215a92ca3ef35bcbbb4674121cb
Kevin Luna <kevin.luna.rdz@gmail.com>
2023-08-24 10:42:05 -0600

    Fin seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/components/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     HeroModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { v4 as uuid } from 'uuid';
+
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[]= [{
+    id: uuid(),
+    name:'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter;
+
+  onDeleteCharacter(id?:string):void{
+    //TODO: Emitir el Id del personaje
+    console.log({id})
+
+    this.onDelete.emit(id);
+
+  }
+
+
+}
